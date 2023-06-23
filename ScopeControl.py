fbe90da8ab44292a234445b2efd2753a63b8aed7
Thorsten Schumacher <thorsten.schumacher@gmx.net>
2023-06-23 15:31:31 +0200

    Update header and licence information to the main python script

--- ScopeControl.py
+++ ScopeControl.py
@@ -1,3 +1,14 @@
+####################################################################################################################################################
+# ScopeControl
+# by Thorsten Schumacher - PrinterLabs
+#
+# Licence: 
+#
+# A slim uvc-camera viewer including basic camera control functions, some live-view filter function and tools for microscopic and astronomic measurements, 
+# frame averaging and a simple timelapse tool. Furthermore it includes the software interface to our DIY 3D-printed microscope, telescope and telescope-mount.
+###################################################################################################################################################
+
+
 # import relevant external modules
 # import required packages
 import cv2     # openCV for live view
