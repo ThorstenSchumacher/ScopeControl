43b5fa1e8b469d9597890fdf7ecf44c5acf805ff
Thorsten Schumacher <thorsten.schumacher@gmx.net>
2023-07-03 16:26:53 +0200

    Version 1.3

--- ScopeControl.py
+++ ScopeControl.py
@@ -2,7 +2,8 @@
 # ScopeControl
 # by Thorsten Schumacher - PrintedLabs
 #
-# Licence: 
+# Licence: Creative Commons Attribution-NonCommercial-ShareAlike (CC BY-NC-SA) 
+# also see: https://creativecommons.org/licenses/by-nc-sa/4.0/deed.de
 #
 # A slim uvc-camera viewer including basic camera control functions, some live-view filter function and tools for microscopic and astronomic measurements, 
 # frame averaging and a simple timelapse tool. Furthermore it includes the software interface to our DIY 3D-printed microscope, telescope and telescope-mount.
